d42e015f82a878a3ca3fc166d6974b64e728c8e0
beepscore <support@beepscore.com>
2014-07-27 12:14:30 -0700

    In TrueFalse add member variables.

--- app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -4,4 +4,17 @@ package com.bignerdranch.android.geoquiz;
  * Created by stevebaker on 7/27/14.
  */
 public class TrueFalse {
+
+    // mQuestion is a resource ID, so type is int not String
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+
+        // m prefix is book's naming convention
+        // alternatively, could use "question" and this.question = question;
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
 }
