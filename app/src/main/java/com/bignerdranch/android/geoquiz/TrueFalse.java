f2d90fea25320a27309e0e9338f0be8bc37f3e3e
beepscore <support@beepscore.com>
2014-07-27 12:42:19 -0700

    In TrueFalse.java use AndroidStudio to generate getters and setters.
    Reference
    http://source.android.com/source/code-style.html#follow-field-naming-conventions
    Alternatively, could use "question" and this.question = question;

--- app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -12,9 +12,28 @@ public class TrueFalse {
 
     public TrueFalse(int question, boolean trueQuestion) {
 
-        // m prefix is book's naming convention
+        // Book follows Android naming convention for m and s prefix
+        // http://source.android.com/source/code-style.html#follow-field-naming-conventions
+        // Can set in Android Studio / Preferences / Code Style / Java / Code Generation
         // alternatively, could use "question" and this.question = question;
         mQuestion = question;
         mTrueQuestion = trueQuestion;
     }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
 }
