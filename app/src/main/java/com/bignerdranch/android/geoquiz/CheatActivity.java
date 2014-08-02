c4ff89cfa7f99a5a634922fdb21e1d02bde84506
beepscore <support@beepscore.com>
2014-08-01 20:40:41 -0700

    In CheatActivity add EXTRA_ANSWER_IS_TRUE.

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
  */
 public class CheatActivity extends Activity {
 
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.beepscore.android.geoquiz.answer_is_true";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
